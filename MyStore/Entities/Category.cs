ffb361365143db058f552dea63abdfaa1e63f23b
yeridi crhis <yeridi crhis@DESKTOP-MHINDNU>
2026-03-17 19:40:19 -0500

    Agregar archivos de proyecto.

---
+++ MyStore/Entities/Category.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MyStore.Entities
+{
+    public class Category
+    {
+
+        public int CategoryId { get; set; }
+        [Required]
+        public string Name { get; set; }
+    }
+}
---
+++ MyStore/Entities/Order.cs
@@ -0,0 +1,12 @@
+﻿namespace MyStore.Entities
+{
+    public class Order
+    {
+        public int OrderId { get; set; }
+        public DateTime OrderDate { get; set; }
+        public int UserId { get; set; }
+        public decimal TotalAmount { get; set; }
+        public User? User { get; set; }
+        public ICollection<OrderItem> OrdenItem { get; set; }
+    }
+}
